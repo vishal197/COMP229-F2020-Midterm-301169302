37acaccef0aa0749fb3228226fb8a7c9170c962b
vishal197 <35914533+vishal197@users.noreply.github.com>
2021-03-06 00:33:23 +0530

    Add files via upload

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports =
+{
+    "URI": "mongodb+srv://vishalpatel97:greatgod123@cluster0.bvitn.mongodb.net/test"
+
+ 
+}
---
+++ server/models/book.js
@@ -0,0 +1,18 @@
+let mongoose = require('mongoose');
+
+// create a model class
+
+let bookModel = mongoose.Schema({
+    name: String,
+    author: String,
+    published : String,
+    description: String,
+    price : Number,
+
+},
+{
+    collection: "book"
+});
+
+
+module.exports = mongoose.model('Book', bookModel);
